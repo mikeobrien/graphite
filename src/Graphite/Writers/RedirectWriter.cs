0a0900e48bae05f83aefe66019eac1d42e152f00
mob <mob@mikeobrien.net>
2017-05-22 18:27:04 -0400

    WIP

--- src/Graphite/Writers/RedirectWriter.cs
+++ src/Graphite/Writers/RedirectWriter.cs
@@ -9,13 +9,52 @@ namespace Graphite.Writers
     public enum RedirectType
     {
         None = 0,
+        /// <summary>
+        /// 301 This response code means that URI of requested resource 
+        /// has been changed. Probably, new URI would be given in the response.
+        /// </summary>
         MovedPermanently = 301,
+        /// <summary>
+        /// 302 This response code means that URI of requested resource has 
+        /// been changed temporarily. New changes in the URI might be made 
+        /// in the future. Therefore, this same URI should be used by the 
+        /// client in future requests.
+        /// </summary>
         Found = 302,
+        /// <summary>
+        /// 303 Server sent this response to directing client to get 
+        /// requested resource to another URI with an GET request.
+        /// </summary>
         SeeOther = 303,
+        /// <summary>
+        /// 304 This is used for caching purposes. It is telling to client that 
+        /// response has not been modified. So, client can continue to 
+        /// use same cached version of response.
+        /// </summary>
         NotModified = 304,
+        /// <summary>
+        /// 305 DEPRICATED Was defined in a previous version of the HTTP specification 
+        /// to indicate that a requested response must be accessed by a proxy. It has 
+        /// been deprecated due to security concerns regarding in-band 
+        /// configuration of a proxy.
+        /// </summary>
         UseProxy = 305,
-        TemporaryKeepVerb = 307,
-        PermanentKeepVerb = 308
+        /// <summary>
+        /// 307	Server sent this response to directing client to get requested resource 
+        /// to another URI with same method that used prior request. This has the same 
+        /// semantic than the 302 Found HTTP response code, with the exception that the 
+        /// user agent must not change the HTTP method used: if a POST was used in 
+        /// the first request, a POST must be used in the second request.
+        /// </summary>
+        TemporaryRedirect = 307,
+        /// <summary>
+        /// 308	This means that the resource is now permanently located at another URI, 
+        /// specified by the Location: HTTP Response header. This has the same semantics 
+        /// as the 301 Moved Permanently HTTP response code, with the exception that the 
+        /// user agent must not change the HTTP method used: if a POST was used in 
+        /// the first request, a POST must be used in the second request.
+        /// </summary>
+        PermanentRedirect = 308
     }
 
     public interface IRedirectable
