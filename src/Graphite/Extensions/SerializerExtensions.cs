0a7d21fbb82f4047f1ee1d0b922e20405204f128
mob <mob@mikeobrien.net>
2017-08-14 16:20:28 -0400

    Modded xml and json readers to throw a bad request exception deserialization errors.

--- src/Graphite/Extensions/SerializerExtensions.cs
+++ src/Graphite/Extensions/SerializerExtensions.cs
@@ -1,15 +1,81 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Reflection;
 using Graphite.Reflection;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 
 namespace Graphite.Extensions
 {
     public static class SerializerExtensions
     {
-        public static JsonSerializerSettings WriteDateTimeAsUtc(this JsonSerializerSettings settings)
+        public class IpAddressConverter : JsonConverter
         {
-            settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(IPAddress);
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                writer.WriteValue(((IPAddress)value).ToString());
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, 
+                object existingValue, JsonSerializer serializer)
+            {
+                return IPAddress.Parse(JToken.Load(reader).Value<string>());
+            }
+        }
+
+        public static JsonSerializerSettings SerializeIpAddresses(this JsonSerializerSettings settings)
+        {
+            settings.Converters.Add(new IpAddressConverter());
+            return settings;
+        }
+
+        public class GraphiteIsoDateTimeConverter : IsoDateTimeConverter
+        {
+            /// <summary>
+            /// Converts to local time after deserialization.
+            /// </summary>
+            public bool AdjustToLocalAfterDeserializing { get; set; }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                var result = base.ReadJson(reader, objectType, existingValue, serializer);
+                if (AdjustToLocalAfterDeserializing && result is DateTime)
+                    return ((DateTime) result).ToLocalTime();
+                return result;
+            }
+        }
+
+        public static JsonSerializerSettings ConfigureIsoDateTimeConverter(this JsonSerializerSettings settings, 
+            Action<GraphiteIsoDateTimeConverter> configure)
+        {
+            settings.RemoveConverters<IsoDateTimeConverter>();
+            configure(settings.GetOrAddConverter<GraphiteIsoDateTimeConverter>());
+            return settings;
+        }
+
+        public static T GetOrAddConverter<T>(this JsonSerializerSettings settings)
+            where T : JsonConverter, new()
+        {
+            var converter = settings.Converters.OfType<T>().FirstOrDefault();
+            if (converter != null) return converter;
+            converter = new T();
+            settings.Converters.Add(converter);
+            return converter;
+        }
+
+        public static JsonSerializerSettings RemoveConverters<T>(this JsonSerializerSettings settings) 
+            where T : JsonConverter
+        {
+            settings.Converters.OfType<T>().ToList()
+                .ForEach(x => settings.Converters.Remove(x));
             return settings;
         }
 
@@ -21,7 +87,7 @@ namespace Graphite.Extensions
 
         public static JsonSerializerSettings WriteEnumNames(this JsonSerializerSettings settings)
         {
-            settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            settings.Converters.Add(new StringEnumConverter());
             return settings;
         }
 
