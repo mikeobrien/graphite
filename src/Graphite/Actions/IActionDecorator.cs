e02a179ae6419402cca03c984ad6e7c024f36d7f
mob <mob@mikeobrien.net>
2017-08-15 14:29:23 -0400

    Added DSL to action decorator context.

--- src/Graphite/Actions/IActionDecorator.cs
+++ src/Graphite/Actions/IActionDecorator.cs
@@ -1,7 +1,19 @@
-﻿using Graphite.Extensibility;
+﻿using System;
+using Graphite.Authentication;
+using Graphite.Behaviors;
+using Graphite.Binding;
+using Graphite.DependencyInjection;
+using Graphite.Extensibility;
+using Graphite.Readers;
+using Graphite.Writers;
 
 namespace Graphite.Actions
 {
+    public interface IActionDecorator : IConditional<ActionDecoratorContext>
+    {
+        void Decorate(ActionDecoratorContext context);
+    }
+
     public class ActionDecoratorContext
     {
         public ActionDecoratorContext(ActionDescriptor actionDescriptor)
@@ -10,10 +22,46 @@ namespace Graphite.Actions
         }
         
         public virtual ActionDescriptor ActionDescriptor { get; }
-    }
 
-    public interface IActionDecorator : IConditional<ActionDecoratorContext>
-    {
-        void Decorate(ActionDecoratorContext context);
+        public ActionDecoratorContext ConfigureRegistry(Action<Registry> configure)
+        {
+            configure(ActionDescriptor.Registry);
+            return this;
+        }
+
+        public ActionDecoratorContext ConfigureAuthenticators(
+            Action<PluginsDsl<IAuthenticator>> configure)
+        {
+            ActionDescriptor.Authenticators.Configure(configure);
+            return this;
+        }
+
+        public ActionDecoratorContext ConfigureRequestBinders(
+            Action<PluginsDsl<IRequestBinder>> configure)
+        {
+            ActionDescriptor.RequestBinders.Configure(configure);
+            return this;
+        }
+
+        public ActionDecoratorContext ConfigureRequestReaders(
+            Action<PluginsDsl<IRequestReader>> configure)
+        {
+            ActionDescriptor.RequestReaders.Configure(configure);
+            return this;
+        }
+
+        public ActionDecoratorContext ConfigureResponseWriters(
+            Action<PluginsDsl<IResponseWriter>> configure)
+        {
+            ActionDescriptor.ResponseWriters.Configure(configure);
+            return this;
+        }
+
+        public ActionDecoratorContext ConfigureBehaviors(
+            Action<PluginsDsl<IBehavior>> configure)
+        {
+            ActionDescriptor.Behaviors.Configure(configure);
+            return this;
+        }
     }
 }
