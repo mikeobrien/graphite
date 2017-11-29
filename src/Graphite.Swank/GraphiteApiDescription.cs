79d6cb308718101607d017079243bb1fdc8b477b
mob <mob@mikeobrien.net>
2017-11-29 10:27:47 -0500

    Misc updates to Swank support.

--- src/Graphite.Swank/GraphiteApiDescription.cs
+++ src/Graphite.Swank/GraphiteApiDescription.cs
@@ -17,6 +17,10 @@ namespace Graphite.Swank
             _actionDescriptor = actionDescriptor;
             HttpMethod = new HttpMethod(actionDescriptor.Route.Method);
             RequestParameter = new GraphiteApiParameter(actionDescriptor.Route.RequestParameter);
+            ParameterDescriptions = actionDescriptor.Route.Parameters
+                .Select(x => new GraphiteApiParameter(x, querystring: true))
+                .Union(_actionDescriptor.Route.UrlParameters
+                    .Select(x => new GraphiteApiParameter(x, urlParameter: true)));
         }
         
         public string Id => _actionDescriptor.Route.Id;
@@ -27,15 +31,10 @@ namespace Graphite.Swank
         public string RelativePath => _actionDescriptor.Route.Url;
         public MethodInfo ActionMethod => _actionDescriptor.Action.MethodDescriptor.MethodInfo;
         public Type ControllerType => _actionDescriptor.Action.HandlerTypeDescriptor.Type;
-        public Type ResponseType => _actionDescriptor.Route.ResponseType.Type;
+        public Type ResponseType => _actionDescriptor.Route.ResponseType?.Type;
         public string ResponseDocumentation { get; } = null;
         public IParameterDescription RequestParameter { get; }
-        
-        public IEnumerable<IParameterDescription> ParameterDescriptions => 
-            _actionDescriptor.Route.Parameters
-                .Select(x => new GraphiteApiParameter(x.ParameterDescriptor, querystring: true))
-                .Union(_actionDescriptor.Route.UrlParameters
-                    .Select(x => new GraphiteApiParameter(x.ParameterDescriptor, urlParameter: true)));
+        public IEnumerable<IParameterDescription> ParameterDescriptions { get; }
 
         public T GetActionAttribute<T>() where T : Attribute =>
             _actionDescriptor.Action.GetAttribute<T>();
